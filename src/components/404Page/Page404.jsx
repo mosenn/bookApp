3ad959e8090abd69ae8571abba9952d40fd831d5
username <mohseen_abasi@yahoo.com>
2023-02-18 21:41:22 +0330

    book app

--- src/components/404Page/Page404.jsx
+++ src/components/404Page/Page404.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { AiFillHome } from "react-icons/ai";
+import "./404.css";
 export const Page404 = () => {
   return (
-    <div>
-      شرمنده صحفه ای که داری دنبالش می گردی نیست
-      <Link to={"/"}>برگرد برو خونه</Link>
+    <div className="main-404">
+      <h1> شرمنده صحفه ای که داری دنبالش می گردی نیست</h1>
+      <Link to={"/"}>
+        <span >
+          <AiFillHome />
+        </span>
+        برگرد برو خونه
+      </Link>
+
+      <figure>
+        <img
+          src="../../../public/img/404-error-with-landscape-concept-illustration_114360-7898.avif"
+          alt=""
+        />
+      </figure>
     </div>
   );
 };
