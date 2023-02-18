4ed8c8555dd866fb4a663c076696eaa638b99260
username <mohseen_abasi@yahoo.com>
2023-02-18 17:51:53 +0330

    book app

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import "./loading.css";
 export const Loading = () => {
   return (
-    <div>
+    <div className="Loading">
       <h1>Loading...</h1>
+      <div></div>
     </div>
   );
 };
